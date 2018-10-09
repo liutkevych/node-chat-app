510235b50c342733d4d121cb0b448fc9192ee947
liutkevych <oli@keepit.com>
2018-10-09 11:11:03 +0300

    newMessage for multiuses

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server!');
-
-    socket.emit('createMessage', {
-        from: 'Oleksandr Liutkevych',
-        text: 'Hey. This is my first message.'
-    });
 });
 
 socket.on('disconnect', function () {

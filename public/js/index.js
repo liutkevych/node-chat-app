a3e3edf148b60ec0671368b665f63ce916dadeac
liutkevych <oli@keepit.com>
2018-10-13 16:55:57 +0300

    Add form to loyaut and show messages in it

--- public/js/index.js
+++ public/js/index.js
@@ -10,4 +10,19 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function(message){
     console.log('New message', message);
+    var li =  $('<li></li>');
+    li.text(`${message.from}: ${message.text}`)
+
+    $('#messages').append(li);
+});
+
+$('#message-form').submit(function(e){
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: $('#message-form [name="message"]').val()
+    }, function(){
+
+    });
 });
